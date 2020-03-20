eb9150329e3317e128424548190cb1a2dc3f259e
nathan-stamand <nathansaint810@gmail.com>
2020-03-20 18:05:03 -0500

    Built scraper for main menu in #get_deals, set up name, details, price variable scrapers.

--- lib/zzounds_deals/scraper.rb
+++ lib/zzounds_deals/scraper.rb
@@ -3,6 +3,16 @@ require 'open-uri'
 class Scraper 
 
     def self.get_deals 
-        doc = Nokogiri::HTML(open('https://www.zzounds.com/blowouts'))
+        doc = Nokogiri::HTML(open('https://www.zzounds.com/blowouts')).css('div#super-hot').css('div.span-63.prepend-top.prepend-1.append-1')
+        i = 0
+        length = doc.css('a').length
+        length.times do
+            name = doc.css('a')[i].text
+            details = doc.css('span.span-15.last.clear')[i].text
+            price = doc.css('div.span-15.last.clear').css('span')[i].inner_text.strip
+            amt_remain = doc.css('div.span-15.last.clear').inner_text.gsub(' ', '').split
+            puts amt_remain
+            i += 1
+        end
     end 
 end 
