24e869ed7ac6068cb0aafea93b3d5e4682c9b851
nathan-stamand <nathansaint810@gmail.com>
2020-03-20 16:25:28 -0500

    Set up #list_items and #menu in CLI class with hard codes

--- lib/zzounds_deals/cli.rb
+++ lib/zzounds_deals/cli.rb
@@ -2,16 +2,20 @@ class ZzoundsDeals::CLI
 
     def call 
         puts "Hello! Here are your daily deals!"
+        list_items
+        menu
+        
+    end 
+
+    def list_items 
         puts "1. Pioneer DJ DDJ-SX3 Professional DJ Controller"
         puts "   $914.95"
         puts "   Blemished"
         puts "   only 1 left!"
         puts "   Save $84.05 off new!"
-        puts "Select the number of the item you would like more information on, type and enter 'list' to return to all of the items, or type and enter 'exit' to leave!"
-        
     end 
 
-    def list_items 
-        
+    def menu 
+        puts "Select the number of the item you would like more information on, type and enter 'list' to return to all of the items, or type and enter 'exit' to leave!"
     end 
 end
