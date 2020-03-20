280a2f746cd47c0bf4218ae58cc39f9d83ec2901
nathan-stamand <nathansaint810@gmail.com>
2020-03-20 15:45:19 -0500

    built out false CLI #call to mimic program.

--- lib/zzounds_deals/cli.rb
+++ lib/zzounds_deals/cli.rb
@@ -1,7 +1,17 @@
 class ZzoundsDeals::CLI
 
     def call 
-        puts "miami's daily traffic"
+        puts "Hello! Here are your daily deals!"
+        puts "1. Pioneer DJ DDJ-SX3 Professional DJ Controller"
+        puts "   $914.95"
+        puts "   Blemished"
+        puts "   only 1 left!"
+        puts "   Save $84.05 off new!"
+        puts "Select the number of the item you would like more information on, type and enter 'list' to return to all of the items, or type and enter 'exit' to leave!"
+        
     end 
 
+    def list_items 
+        
+    end 
 end
