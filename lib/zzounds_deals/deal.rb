faf05e4f4979140d4c3bd20153563022733e8d2f
nathan-stamand <nathansaint810@gmail.com>
2020-03-20 17:14:00 -0500

    added attr_accessors for Deal class and #initialize, #all methods for Deal

--- lib/zzounds_deals/deal.rb
+++ lib/zzounds_deals/deal.rb
@@ -1,3 +1,19 @@
 class ZzoundsDeals::Deal
+    attr_accessor :name, :price, :details, :amt_remain, :url, :savings
+    @@all = []
+
+    def initialize(name, price, details, amt_remain, url, savings)
+        @name = name 
+        @price = price 
+        @details = details 
+        @amt_remain = amt_remain 
+        @url = url 
+        @savings = savings 
+        @@all << self
+    end
+      
+    def self.all 
+        @@all 
+    end
 
 end
