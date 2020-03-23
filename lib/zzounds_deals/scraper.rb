92a97095e045c574431c458cdcf94bb0424fca3e
nathan-stamand <nathansaint810@gmail.com>
2020-03-23 08:42:38 -0500

    Scrapes first dataset from Zzounds site

--- lib/zzounds_deals/scraper.rb
+++ lib/zzounds_deals/scraper.rb
@@ -7,11 +7,16 @@ class Scraper
         i = 0
         length = doc.css('a').length
         length.times do
+
             name = doc.css('a')[i].text
             details = doc.css('span.span-15.last.clear')[i].text
             price = doc.css('div.span-15.last.clear').css('span')[i].inner_text.strip
-            amt_remain = doc.css('div.span-15.last.clear').inner_text.gsub(' ', '').split
-            puts amt_remain
+            amt_remain = doc.css('div.span-15.last.bd')[i].text.split.select{|x| x.to_i > 0 && x.length == 1}[0]
+            savings = doc.css('div.span-15.last.clear')[i].inner_text.split.select{|x| x[0] =='$'}[0]
+            url = doc.css('a')[i].attr('href')
+    
+            ZzoundsDeals::Deal.new(name, price, details, amt_remain, url, savings)
+
             i += 1
         end
     end 
